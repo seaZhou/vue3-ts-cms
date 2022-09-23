cfeec863f55d013146acc4f269b3d6a4b865a43a
jason <1171161083@qq.com>
2022-09-23 17:13:20 +0800

    feat: eslint 检测

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="overview">2132</div>
+  <div class="overview"></div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'overview',
+  setup() {
+    return {}
+  }
+})
+</script>
